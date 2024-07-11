4c85c429f846c841f92c5e7408fc36226d1c0710
Ryan Hanlon <ryan.hanlon@hotmail.co.uk>
2024-07-12 00:52:58 +0100

    Less rear vulnerability

--- sandboxvars.lua
+++ sandboxvars.lua
@@ -407,7 +407,7 @@ SandboxVars = {
     -- Chance of being bitten when a zombie attacks from behind. Default=High
     -- 1 = Low
     -- 2 = Medium
-    RearVulnerability = 3,
+    RearVulnerability = 2,
     -- Disable to walk unimpeded while melee attacking.
     AttackBlockMovements = true,
     AllClothesUnlocked = false,
